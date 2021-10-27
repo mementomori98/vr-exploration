eb5f577c22d62a470e0e532821a3ced2c66000e2
mementomori98 <sebestyen.mate98@gmail.com>
2021-10-27 23:42:53 +0200

    stuff

--- Assets/Scripts/ContinuousFlightProvider.cs
+++ Assets/Scripts/ContinuousFlightProvider.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR;
 
-public class FlightController : MonoBehaviour
+public class ContinuousFlightProvider : MonoBehaviour
 {
 
     public InputActionReference action;
